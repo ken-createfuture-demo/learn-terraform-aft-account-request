5aa91698a8a31a3b7273b212635c9d297e01feec
Chukwu Emmanuel <eechukwu@gmail.com>
2026-02-06 18:01:19 +0000

    Testing account creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,29 @@
 module "sandbox_approval_gates_test_final" {
   source = "./modules/aft-account-request"
-  
+
   control_tower_parameters = {
-    AccountEmail              = "keneaft+approvalgatesgiveup@gmail.com"
-    AccountName               = "aft-approval-gates-test-giveup2025"
+    AccountEmail              = "keneaft+accountrequest@gmail.com"
+    AccountName               = "aft-account-request"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "keneaft@gmail.com"
     SSOUserFirstName          = "Kene"
     SSOUserLastName           = "AFT"
   }
-  
+
   account_tags = {
     Environment = "Testing"
     Purpose     = "ApprovalGatesFinalValidation"
   }
-  
+
   change_management_parameters = {
     change_requested_by = "kene.aft"
     change_reason       = "Final validation of approval gates implementation"
   }
-  
+
   custom_fields = {
-    test_type      = "approval-gates-validation"
-    deployment     = "fresh-aft-with-correct-ssm"
+    test_type  = "approval-gates-validation"
+    deployment = "fresh-aft-with-correct-ssm"
   }
-  
+
   account_customizations_name = "sandbox"
 }
